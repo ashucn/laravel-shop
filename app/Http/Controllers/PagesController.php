b577bbff7382ef70c17f3b3df149784d4e1a6fbf
MayLiu <ashucn@gmail.com>
2018-12-07 23:54:26 -0800

    middleware for email verified

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,8 +7,20 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
 
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function root()
     {
         return view('pages.root');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function emailVerifyNotice(Request $request)
+    {
+        return view('pages.email_verify_notice');
     }
 }
