29d5e4673496eb12918bffd40912abdcbeb56ec3
MayLiu <ashucn@gmail.com>
2018-12-09 20:14:25 -0800

    user interface: products list

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -31,5 +32,22 @@ class Product extends Model
     public function skus()
     {
         return $this->hasMany(ProductSku::class);
+    }
+
+    /**
+     * 商品的 image 字段保存的是图片的相对于目录 storage/app/public/ 的路径，
+     * 需要转成绝对路径才能正常展示，
+     * 我们可以给商品模型加一个访问器来输出绝对路径
+     * 这里 \Storage::disk('public') 的参数 public 需要和我们在 config/admin.php 里面的 upload.disk 配置一致
+     *
+     * @return mixed
+     */
+    public function getImageUrlAttribute()
+    {
+        // 如果 image 字段本身就已经是完整的 url 就直接返回
+        if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
+            return $this->attributes['image'];
+        }
+        return \Storage::disk('public')->url($this->attributes['image']);
     }
 }
