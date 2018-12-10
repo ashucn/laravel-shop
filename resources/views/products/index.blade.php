e903d891849142679fc66db57ae289515728ca9c
MayLiu <ashucn@gmail.com>
2018-12-09 21:22:30 -0800

    add laravel-debugbar; user interface: products filter and order

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,6 +6,25 @@
         <div class="col-lg-10 col-lg-offset-1">
             <div class="panel panel-default">
                 <div class="panel-body">
+
+                    {{--<!-- 筛选组件开始 -->--}}
+                    <div class="row">
+                        <form action="{{ route('products.index') }}" class="form-inline search-form">
+                            <input type="text" class="form-control input-sm" name="search" placeholder="搜索">
+                            <button class="btn btn-primary btn-sm">搜索</button>
+                            <select name="order" class="form-control input-sm pull-right">
+                                <option value="">排序方式</option>
+                                <option value="price_asc">价格从低到高</option>
+                                <option value="price_desc">价格从高到低</option>
+                                <option value="sold_count_desc">销量从高到低</option>
+                                <option value="sold_count_asc">销量从低到高</option>
+                                <option value="rating_desc">评价从高到低</option>
+                                <option value="rating_asc">评价从低到高</option>
+                            </select>
+                        </form>
+                    </div>
+                    {{--<!-- 筛选组件结束 -->--}}
+
                     <div class="row products-list">
                         @foreach($products as $product)
                             <div class="col-xs-3 product-item">
@@ -23,10 +42,25 @@
                             </div>
                         @endforeach
                     </div>
-{{--pagination--}}
-                    <div class="pull-right">{{ $products->render() }}</div>
+                    {{--pagination start--}}
+                    {{--appends() 方法接受一个 Key-Value 形式的数组作为参数，在生成分页链接的时候会把这个数组格式化成查询参数。--}}
+                    <div class="pull-right">{{ $products->appends($filters)->render() }}</div>
+                    {{--pagination end--}}
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('scriptsAfterJs')
+    <script>
+      var filters = {!! json_encode($filters) !!};
+      $(document).ready(function () {
+        $('.search-form input[name=search]').val(filters.search);
+        $('.search-form select[name=order]').val(filters.order);
+        $('.search-form select[name=order]').on('change', function() {
+          $('.search-form').submit();
+        });
+      })
+    </script>
 @endsection
