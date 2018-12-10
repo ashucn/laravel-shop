f1a119fcf84711b291776937340807703d71283d
MayLiu <ashucn@gmail.com>
2018-12-10 00:26:17 -0800

    create order

--- app/Models/ProductSku.php
+++ app/Models/ProductSku.php
@@ -3,13 +3,52 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Exceptions\InternalException;
 
 class ProductSku extends Model
 {
+
     protected $fillable = ['title', 'description', 'price', 'stock'];
 
     public function product()
     {
         return $this->belongsTo(Product::class);
+    }
+
+    // 减少库存
+
+    /**
+     * 为何使用 数据库查询构造器 $this->newQuery() ？
+     * ORM 查询构造器的写操作只会返回 true 或者 false 代表 SQL 是否执行成功，
+     * 而数据库查询构造器的写操作则会返回影响的行数。
+     * 最终执行的 SQL 类似于 update product_skus set stock = stock - $amount where id = $id and stock >= $amount，
+     * 这样可以保证不会出现执行之后 stock 值为负数的情况，也就避免了超卖的问题。
+     * 而且我们用了数据库查询构造器，可以通过返回的影响行数来判断减库存操作是否成功，
+     * 如果不成功说明商品库存不足。
+     * 如果减库存失败则抛出异常，由于这块代码是在 DB::transaction() 中执行的，因此抛出异常时会触发事务的回滚，
+     * 之前创建的 orders 和 order_items 记录都会被撤销。
+     */
+    public function decreaseStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('减库存不可小于0');
+        }
+
+        return $this->newQuery()->where('id', $this->id)->where('stock', '>=', $amount)->decrement('stock', $amount);
+    }
+
+    // 增加库存
+
+    /**
+     * 通过 increment() 方法来保证操作的原子性。
+     * @param $amount
+     * @throws InternalException
+     */
+    public function addStock($amount)
+    {
+        if ($amount < 0) {
+            throw new InternalException('加库存不可小于0');
+        }
+        $this->increment('stock', $amount);
     }
 }
--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -23,17 +23,20 @@
                             {{--把 SKU 的 ID 作为 <tr> 标签的 data-id 属性的值，这是为了之后删除商品或者提交订单时能够取得对应的 ID。--}}
                             <tr data-id="{{ $item->productSku->id }}">
                                 <td>
-                                    <input type="checkbox" name="select" value="{{ $item->productSku->id }}" {{ $item->productSku->product->on_sale ? 'checked' : 'disabled' }}>
+                                    <input type="checkbox" name="select"
+                                           value="{{ $item->productSku->id }}" {{ $item->productSku->product->on_sale ? 'checked' : 'disabled' }}>
                                 </td>
                                 <td class="product_info">
                                     <div class="preview">
-                                        <a target="_blank" href="{{ route('products.show', [$item->productSku->product_id]) }}">
+                                        <a target="_blank"
+                                           href="{{ route('products.show', [$item->productSku->product_id]) }}">
                                             <img src="{{ $item->productSku->product->image_url }}">
                                         </a>
                                     </div>
                                     <div @if(!$item->productSku->product->on_sale) class="not_on_sale" @endif>
               <span class="product_title">
-                <a target="_blank" href="{{ route('products.show', [$item->productSku->product_id]) }}">{{ $item->productSku->product->title }}</a>
+                <a target="_blank"
+                   href="{{ route('products.show', [$item->productSku->product_id]) }}">{{ $item->productSku->product->title }}</a>
               </span>
                                         <span class="sku_title">{{ $item->productSku->title }}</span>
                                         @if(!$item->productSku->product->on_sale)
@@ -43,7 +46,9 @@
                                 </td>
                                 <td><span class="price">￥{{ $item->productSku->price }}</span></td>
                                 <td>
-                                    <input type="text" class="form-control input-sm amount" @if(!$item->productSku->product->on_sale) disabled @endif name="amount" value="{{ $item->amount }}">
+                                    <input type="text" class="form-control input-sm amount"
+                                           @if(!$item->productSku->product->on_sale) disabled @endif name="amount"
+                                           value="{{ $item->amount }}">
                                 </td>
                                 <td>
                                     <button class="btn btn-xs btn-danger btn-remove">移除</button>
@@ -52,6 +57,34 @@
                         @endforeach
                         </tbody>
                     </table>
+                    {{--<!-- 开始 -->--}}
+                    <div>
+                        <form class="form-horizontal" role="form" id="order-form">
+                            <div class="form-group">
+                                <label class="control-label col-sm-3">选择收货地址</label>
+                                <div class="col-sm-9 col-md-7">
+                                    <select class="form-control" name="address">
+                                        @foreach($addresses as $address)
+                                            <option
+                                                value="{{ $address->id }}">{{ $address->full_address }} {{ $address->contact_name }} {{ $address->contact_phone }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-sm-3">备注</label>
+                                <div class="col-sm-9 col-md-7">
+                                    <textarea name="remark" class="form-control" rows="3"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-3">
+                                    <button type="button" class="btn btn-primary btn-create-order">提交订单</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    {{--<!-- 结束 -->--}}
                 </div>
             </div>
         </div>
@@ -60,28 +93,26 @@
 
 @section('scriptsAfterJs')
     <script>
-      $(document).ready(function () {
+      $(document).ready(function() {
         // 监听 移除 按钮的点击事件
-        $('.btn-remove').click(function () {
+        $('.btn-remove').click(function() {
           // $(this) 可以获取到当前点击的 移除 按钮的 jQuery 对象
           // closest() 方法可以获取到匹配选择器的第一个祖先元素，在这里就是当前点击的 移除 按钮之上的 <tr> 标签
           // data('id') 方法可以获取到我们之前设置的 data-id 属性的值，也就是对应的 SKU id
           var id = $(this).closest('tr').data('id');
           swal({
-            title: "确认要将该商品移除？",
-            icon: "warning",
+            title: '确认要将该商品移除？',
+            icon: 'warning',
             buttons: ['取消', '确定'],
             dangerMode: true,
-          })
-          .then(function(willDelete) {
+          }).then(function(willDelete) {
             // 用户点击 确定 按钮，willDelete 的值就会是 true，否则为 false
             if (!willDelete) {
               return;
             }
-            axios.delete('/cart/' + id)
-            .then(function () {
+            axios.delete('/cart/' + id).then(function() {
               location.reload();
-            })
+            });
           });
         });
 
@@ -98,5 +129,54 @@
           });
         });
       });
+
+      // 监听创建订单按钮的点击事件
+      $('.btn-create-order').click(function() {
+        // 构建请求参数，将用户选择的地址的 id 和备注内容写入请求参数
+        var req = {
+          address_id: $('#order-form').find('select[name=address]').val(),
+          items: [],
+          remark: $('#order-form').find('textarea[name=remark]').val(),
+        };
+        // 遍历 <table> 标签内所有带有 data-id 属性的 <tr> 标签，也就是每一个购物车中的商品 SKU
+        $('table tr[data-id]').each(function() {
+          // 获取当前行的单选框
+          var $checkbox = $(this).find('input[name=select][type=checkbox]');
+          // 如果单选框被禁用或者没有被选中则跳过
+          if ($checkbox.prop('disabled') || !$checkbox.prop('checked')) {
+            return;
+          }
+          // 获取当前行中数量输入框
+          var $input = $(this).find('input[name=amount]');
+          // 如果用户将数量设为 0 或者不是一个数字，则也跳过
+          if ($input.val() == 0 || isNaN($input.val())) {
+            return;
+          }
+          // 把 SKU id 和数量存入请求参数数组中
+          req.items.push({
+            sku_id: $(this).data('id'),
+            amount: $input.val(),
+          });
+        });
+        axios.post('{{ route('orders.store') }}', req).then(function(response) {
+          swal('订单提交成功', '', 'success');
+        }, function(error) {
+          if (error.response.status === 422) {
+            // http 状态码为 422 代表用户输入校验失败
+            var html = '<div>';
+            _.each(error.response.data.errors, function(errors) {
+              _.each(errors, function(error) {
+                html += error + '<br>';
+              });
+            });
+            html += '</div>';
+            swal({content: $(html)[0], icon: 'error'});
+          } else {
+            // 其他情况应该是系统挂了
+            swal('系统错误', '', 'error');
+          }
+        });
+      });
+
     </script>
 @endsection
